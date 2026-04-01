d7a8ead3498743f8427828cb5dfa784fc562d54d
Christian Wu <chrwu@g.hmc.edu>
2026-03-31 18:28:23 -0700

    changed ifu to integrate StallF and support the new variable names that are used for pipelining

--- examples/exercises/lynn/sample_processor/src/ifu.sv
+++ examples/exercises/lynn/sample_processor/src/ifu.sv
@@ -4,31 +4,27 @@
 
 module ifu(
         input   logic           clk, reset,
-        input   logic           PCSrc,
-        input   logic [31:0]    IEUAdr,
-        output  logic [31:0]    PC, PCPlus4
+        input   logic           PCSrcE,
+        input   logic           PCTargetE,
+        input   logic           StallF,
+        output  logic [31:0]    PCF, PCPlus4F
     );
 
     logic [31:0] PCNext;
-    logic [31:0] BranchTarget;
-
-    assign BranchTarget = IEUAdr & 32'hFFFFFFFE;
-
     logic [31:0] entry_addr;
 
     initial begin
         entry_addr = '0;
-
         void'($value$plusargs("ENTRY_ADDR=%h", entry_addr));
-
         $display("[TB] ENTRY_ADDR = 0x%h", entry_addr);
     end
 
     always_ff @(posedge clk) begin
-        if (reset)  PC <= entry_addr;
-        else        PC <= PCNext;
+        if      (reset)   PCF <= entry_addr;
+        else if (~StallF) PCF <= PCNext;
     end
 
-    adder pcadd4(PC, 32'd4, PCPlus4);
-    mux2 #(32) pcmux(PCPlus4, BranchTarget, PCSrc, PCNext);
+    adder pcadd4(PCF, 32'd4, PCPlus4F);
+
+    mux2 #(32) pcmux(PCPlus4F, PCTargetE, PCSrcE, PCNext);
 endmodule
