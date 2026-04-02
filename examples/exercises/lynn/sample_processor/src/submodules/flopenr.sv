eb1d8351b5a38b538714d0a214d64241519e6c83
Christian Wu <chrwu@g.hmc.edu>
2026-04-01 18:33:55 -0700

    modified flopenr to have a asynchronous reset like flopr and have default reset parameter

--- examples/exercises/lynn/sample_processor/src/submodules/flopenr.sv
+++ examples/exercises/lynn/sample_processor/src/submodules/flopenr.sv
@@ -3,14 +3,14 @@
 // 03/30/2026
 // chrwu@g.hmc.edu eoo@g.hmc.edu
 
-module flopenr #(parameter WIDTH) (
+module flopenr #(parameter WIDTH, parameter DEFAULT = 0) (
     input  logic             clk,
     input  logic             reset,
     input  logic             en,      
     input  logic [WIDTH-1:0] D,
     output logic [WIDTH-1:0] Q
 );
-    always_ff @(posedge clk)
-        if      (reset) Q <= 0;
+    always_ff @(posedge clk, posedge reset)
+        if      (reset) Q <= DEFAULT;
         else if (en)    Q <= D;
 endmodule
